17f920f3c80c3d9e3b6d7bf887bea6eb37926895
YuleisyQuipuzcoa22 <126414194+YuleisyQuipuzcoa22@users.noreply.github.com>
2023-03-05 01:03:45 -0500

    Delete ejercicio2.py

--- ejercicio2.py
+++
@@ -1,8 +0,0 @@
-#QUIPUZCOA LOPEZ YULEISY
-print(" ÁREAS DE FIGURAS GEOMÉTRICAS")
-print("╚════════════════════════════╝")
-num= float(input("Ingresa un valor numérico para hallar el área de diversas figuras: "))
-print("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨")
-print("Área de un círculo: ", 3.14*(num)**2)
-print("Área de un triángulo: ", (num*num)/2)
-print("Área de un cuadrado: ", num**2)
