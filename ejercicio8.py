89fc28310f2b4cae1771bb1dab11a436201323ef
YuleisyQuipuzcoa22 <126414194+YuleisyQuipuzcoa22@users.noreply.github.com>
2023-03-05 01:04:52 -0500

    Delete ejercicio8.py

--- ejercicio8.py
+++
@@ -1,9 +0,0 @@
-#QUIPUZCOA LOPEZ YULEISY
-passw= "contraseña"
-contra= " "
-contra=(input("Introduce la contraseña: "))
-contra=contra.lower()
-if(contra== passw):
-    print("Contraseña correcta.")
-else:
-    print("Contraseña incorrecta.")
