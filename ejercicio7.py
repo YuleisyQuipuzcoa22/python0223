f0ecac1c69b4bd63d8faa7853b7eaea12658ee5b
YuleisyQuipuzcoa22 <126414194+YuleisyQuipuzcoa22@users.noreply.github.com>
2023-03-05 01:04:43 -0500

    Delete ejercicio7.py

--- ejercicio7.py
+++
@@ -1,7 +0,0 @@
-#QUIPUZCOA LOPEZ YULEISY
-num1=float(input("Ingresa un primer número: "))
-num2=float(input("Ingresa un segundo número: "))
-print("1)¿Los dos números son iguales?\n -",num1==num2)
-print("2)¿Los dos números son diferentes?\n -",num1!=num2)
-print("3)¿",num1, "es mayor que ", num2, "?\n -", num1>num2)
-print("4)¿",num2, "es mayor o igual que ", num1, "?\n -", num2>= num1)
